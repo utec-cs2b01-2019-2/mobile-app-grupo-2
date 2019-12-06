b4ad51ac21388067234d28ee680d8b255416ffaf
juliobonifaz <42589776+juliobonifaz@users.noreply.github.com>
2019-12-06 16:54:39 -0500

    commit

---
+++ code/Paginamobile/app/src/main/java/cs2b01/utec/chat_mobile/LoginActivity.java
@@ -0,0 +1,94 @@
+package cs2b01.utec.chat_mobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void onBtnLoginClicked(View view){
+        //1.  Getting username and password (from the view)
+        EditText txtUsername = (EditText)findViewById(R.id.txtUsername);
+        EditText txtPassword = (EditText)findViewById(R.id.txtPassword);
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        //2.  Creating a message using user input
+        Map<String, String> message = new HashMap<>();
+        message.put("username", username);
+        message.put("password", password);
+
+        //3.  Converting the message object to JSON string (jsonify)
+        JSONObject jsonMessage = new JSONObject(message);
+
+
+        //4.  Sending json message to the server
+        //4.1. Install volley
+        //4.2. Create request object
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                "http://10.0.2.2:5000/authenticate",
+                jsonMessage,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //TODO Qué hacer cuando el server responda
+                        showMessage("Authorized!");
+                        try {
+                            String username = response.getString("username");
+                            int user_id = response.getInt("user_id");
+                            goToContactsActivity(user_id, username);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO Qué hacer cuando ocurra un error
+                        showMessage("Unauthorized!!!");
+                    }
+                }
+        );
+
+        //5. Send Request to the Server
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+
+    }
+
+    private void goToContactsActivity(int user_id, String username) {
+        Intent intent = new Intent(this, WordActivity.class);
+        intent.putExtra("user_id", user_id);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
+}
---
+++ code/Paginamobile/app/src/main/java/cs2b01/utec/chat_mobile/MessageActivity.java
@@ -0,0 +1,140 @@
+package cs2b01.utec.chat_mobile;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class MessageActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        String username = getIntent().getExtras().getString("username");
+        setTitle("@"+username);
+        mRecyclerView = findViewById(R.id.main_recycler_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        getMessages();
+    }
+
+    public Activity getActivity(){
+        return this;
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void getMessages(){
+        double random = Math.floor(Math.random() * (8-1)+1 );
+        final int userFromId = getIntent().getExtras().getInt("user_from_id");
+
+        String uri = "http://10.0.2.2:5000/words/"+random;
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                uri,
+                null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //TODO process response
+                        mAdapter = new MessageAdapter(response, getActivity(), userFromId);
+                        mRecyclerView.setAdapter(mAdapter);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+
+
+    }
+
+    public void onClickBtnSend(View view) {
+        final int userFromId = getIntent().getExtras().getInt("user_from_id");
+
+        EditText txtMessage = (EditText) findViewById(R.id.txtMessage);
+        String Message = txtMessage.getText().toString();
+
+        Map<String, String> message = new HashMap<>();
+        message.put("response", txtMessage.toString());
+        message.put("userFromId", Integer.toString(userFromId));
+
+
+        JSONObject jsonMessage = new JSONObject(message);
+
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                "http://10.0.2.2:80/gabriel/word",
+                jsonMessage,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //TODO Qué hacer cuando el server responda
+                        showMessage("Send");
+                        try {
+                            String message = response.getString("message");
+                            String response2 = response.getString("reponse");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO Qué hacer cuando ocurra un error
+                        showMessage("fail to send");
+                    }
+                }
+        );
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+
+
+
+    }
+
+}
